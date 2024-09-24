a7de3a153317268c37df9b80738f5a789c766fea
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-24 21:15:42 +0300

    edit evaluate

--- evaluate.py
+++ evaluate.py
@@ -1,20 +1,18 @@
 import torch
 
-def Evaluate(dataloader,model,criterion,optimizer,device):
+def Evaluate(dataloader,model,criterion,device):
     model.eval()
     total_loss=0
     correct=0
     with torch.no_grad():
-     for batch in dataloader:
-        input_features,labels=batch
-        input_features,labels=input_features.to(device),labels.to(device)
-        optimizer.zero_grad()
-        outputs=model(input_features)
-        loss=criterion(outputs,labels)
-        loss.backward()
-        optimizer.step()
-        total_loss+=loss.item()
-        predictions = torch.argmax(outputs, dim=1)
-        correct += (predictions == labels).sum().item()
+        for batch in dataloader:
+            input_features, labels = batch
+            input_features, labels = input_features.to(device), labels.to(device)
+            outputs = model(input_features)
+            loss = criterion(outputs, labels)
+            total_loss += loss.item()
+            predictions = torch.argmax(outputs, dim=1)
+            correct += (predictions == labels).sum().item()
     accuracy = correct / len(dataloader.dataset)
     return total_loss / len(dataloader), accuracy
+  
